afcfbb57064d9d892d6d448d8c6a213e063820b3
Ji One Yang <jioneyang@Jis-MacBook-Pro.local>
2020-03-27 00:31:28 -0700

    Adding hw and lab directory along with lab00

---
+++ lab/lab00/lab00.py
@@ -0,0 +1,21 @@
+def twenty_twenty():
+    """Come up with the most creative expression that evaluates to 2020,
+    using only numbers and the +, *, and - operators.
+
+    >>> twenty_twenty()
+    2020
+    """
+    return _____
+
+def whatsmyname():
+    """ Make the function return your name."""
+    return _____
+
+def main():
+    print("My name is: " + whatsmyname())
+    print("Making sure twenty_twenty returns 2020")
+    assert(twenty_twenty() == 2020)
+    print("Nice... it did")
+
+if __name__ == "__main__":
+    main()
